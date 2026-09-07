565609c3fcbb652642a1c5317d4fd52fb5ed3437
VorinthexAI <oscar@vorinthex.com>
2026-09-07 22:24:57 +0200

    fix(infra): allow canonical asset publishing

---
+++ terraform/environments/production/deploy_iam.tf
@@ -0,0 +1,17 @@
+data "aws_iam_role" "backend_deploy" {
+  name = "vorinthex-backend-deploy"
+}
+
+resource "aws_iam_role_policy" "backend_deploy_system_assets" {
+  name = "vorinthex-backend-deploy-system-assets"
+  role = data.aws_iam_role.backend_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:PutObject"]
+      Resource = ["${module.storage.s3_bucket_arn}/apps/logos/v1/*"]
+    }]
+  })
+}
